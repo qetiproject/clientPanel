8da27e1ed9ac10c67a5792fa3e11d5ae1d89357c
keti khetsuriani <k.khetsuriani@betsolutions.com>
2019-10-04 11:12:07 +0400

    Auth guard for routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,15 +7,16 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { EditClientComponent } from './components/edit-client/edit-client.component';
+import { AuthGuard } from './guards/auth.guards';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent },
+  { path: '', component: DashboardComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'client/add', component: AddClientComponent },
-  { path: 'client/edit/:id', component: EditClientComponent },
-  { path: 'client/:id', component: ClientDetailsComponent },
-  { path: 'settings', component: SettingsComponent }
+  { path: 'client/add', component: AddClientComponent, canActivate: [AuthGuard] },
+  { path: 'client/edit/:id', component: EditClientComponent, canActivate: [AuthGuard] },
+  { path: 'client/:id', component: ClientDetailsComponent, canActivate: [AuthGuard] },
+  { path: 'settings', component: SettingsComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
@@ -23,6 +24,6 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(routes)
   ],
-  declarations: []
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }
